4a061698d19fa0f846425baeba3f0cee0533ef8c
StuFF mc <m+git@carrascomolina.com>
2020-04-12 22:03:01 +0200

    More fine tuning also for Dark Mode

--- TabbedViews/ContentView.swift
+++ TabbedViews/ContentView.swift
@@ -57,6 +57,8 @@ public struct HoverRecognizer: UIViewRepresentable {
 
 struct ContentView: View {
     @State private var tabs = [Date()]
+    @Environment (\.colorScheme) var colorScheme: ColorScheme
+    
     @State private var currentTabIndex = 0
     @State private var hoveredTabIndex = -1
     
@@ -70,44 +72,56 @@ struct ContentView: View {
         GeometryReader { geo in
             VStack {
                 HStack {
+                    Spacer(minLength: 0.5)
                     ForEach((0..<self.tabs.count), id: \.self) { index in
                         Button(action: {
                             self.currentTabIndex = index
                         }) {
-                            HStack {
-                                Button(action: {
-                                    self.tabs.remove(at: index)
-                                }) {
-                                    Image(systemName: "xmark")
-                                }
-                                .padding(5)
-                                .background(Color.gray)
-                                .mask(RoundedRectangle(cornerRadius: 2))
-                                .hoverEffect(.highlight)
-                                .opacity(self.hoveredTabIndex == index ? 1 : 0)
-                                .animation(.default)
+                            ZStack {
                                 Text(self.formatter.string(from: self.tabs[index]))
+                                HStack() {
+                                    Button(action: {
+                                        self.tabs.remove(at: index)
+                                    }) {
+                                        Image(systemName: "xmark")
+                                    }
+                                    .padding(5)
+                                    .background(Color(white: 0.53))
+                                    .background(self.hoveredCloseButton ? Color(white: self.colorScheme == .dark ? 0.14 : 0.53) : self.tabColor(index == self.currentTabIndex))
+                                    .mask(RoundedRectangle(cornerRadius: 3))
+                                    .padding(.leading, 5)
+                                    .hoverEffect(.highlight)
+                                    .opacity(self.hoveredTabIndex == index ? 1 : 0)
+                                    .animation(.default)
+                                    Spacer()
+                                }
                             }
-                            .frame(width: (geo.size.width - 20) / CGFloat(self.tabs.count), height: 30)
+                            .frame(height: 30)
+                            .padding(0)
                         }
                         .onHover2 {
                             self.hoveredTabIndex = $0 ? index : -1
                         }
-                        .padding(0)
-                        .background(Color(white: index == self.currentTabIndex ? 0.81 : 0.74))
+                        .background(self.tabColor(index == self.currentTabIndex))
                         .foregroundColor(.primary)
                         .border(Color.gray.opacity(0.7), width: 1)
                     }
                     AddNewTabButton {
                         self.tabs.append(Date())
                     }
-                    Spacer()
+                    .padding(.trailing, 10)
                 }
-                .frame(height: 20)
                 DateView(date: self.tabs[self.currentTabIndex])
                 Spacer()
             }
+            .padding(0)
         }
+    }
+    
+    func tabColor(_ isCurrentTabIndex: Bool) -> Color {
+        // Yes, those colors are hard coded to reflect the Safari colors. Tell me if you find a better way.
+        // One of them is `labelColor` and another one `gridColor` but I don't know a way to refer them and anyways there's no grid or label here.
+        Color(white: isCurrentTabIndex ? (colorScheme == .dark ? 0.16 : 0.81) : (colorScheme == .dark ? 0.09 : 0.74))
     }
 }
 
@@ -122,6 +136,7 @@ struct AddNewTabButton: View {
             Image(systemName: "plus")
         }
         .frame(width: 20)
+        .foregroundColor(.primary)
     }
 }
 
@@ -135,11 +150,18 @@ struct DateView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
+    let w = 1024
+    let h = 100
+    
     static var previews: some View {
-        ContentView()
-            .previewLayout(.fixed(width: 1024, height: 200))
-            .background(Color(UIColor.systemBackground))
-            .environment(\.colorScheme, .dark)
-        
+        Group {
+            ContentView()
+                .previewLayout(.fixed(width: 520, height: 100))
+                .environment(\.colorScheme, .light)
+
+//            ContentView()
+//                .previewLayout(.fixed(width: 1024, height: 100))
+//                .environment(\.colorScheme, .dark)
+        }
     }
 }
